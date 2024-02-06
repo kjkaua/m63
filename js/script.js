cfbd07608d8d677f763d6210e00bfbfb5c4afd51
Kaua Reymond Ratos e Fofos da Silva (Desgraça`s Version) <159155314+kjkaua@users.noreply.github.com>
2024-02-06 12:25:46 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function teste(){
+
+    let nome = documet.getElementById("nome").value;
+    let senha = document.getElementById("senha").value;
+
+    if(nome != "kaua" && senha != "123"){
+        alert("Usuario e senha invalidos");
+    }
+    alert(nome);
+    alert(senha);
+}
